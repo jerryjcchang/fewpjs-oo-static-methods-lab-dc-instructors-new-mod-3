a09a3bff32f45d6629b733174fff7ad9ece1d491
JC Chang <jerry.chang@flatironschool.com>
2019-05-16 10:53:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace( /[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string){
+    console.log(string)
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    function formatWord(word){
+      return (exceptions.includes(word) ? word : word.charAt(0).toUpperCase()+word.slice(1))
+    }
+    let arr = string.split(" ")
+    return this.capitalize(arr[0])+" "+arr.slice(1).map(formatWord).join(" ")
+  }
 }
